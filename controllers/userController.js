f296fc4eff3679ce3ce317abeb724e9d715aa210
Arun Kumar Bidila <arunkumarbidila@gmail.com>
2026-02-12 18:00:46 +0530

    feat/getuser

--- controllers/userController.js
+++ controllers/userController.js
@@ -67,4 +67,26 @@ const loginUser = async (req, res) => {
   }
 };
 
-export { signUpUser,loginUser };
+
+const getUser=async (req,res)=>{
+  try {
+
+    const user=await User.findById(req.userId).select("-password");
+
+    if(!user){
+      return res.status(404).json({message:"User not found"})
+    }
+
+    return res.status(200).json(user);
+
+
+
+    
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({message:"Server error"});
+    
+  }
+}
+
+export { signUpUser,loginUser,getUser };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { signUpUser ,loginUser} from "../controllers/userController.js";
+import { signUpUser ,loginUser, getUser} from "../controllers/userController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 const userRouter=express.Router();
 
 userRouter.post("/signup",signUpUser);
 userRouter.post("/login",loginUser);
+userRouter.get("/getuser",authMiddleware,getUser)
 
 
 export default userRouter;
