45fd2d8ed0934511f32ab6180cae21479eeaf891
Arun Kumar Bidila <arunkumarbidila@gmail.com>
2026-02-20 12:44:56 +0530

    change password api

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,43 +50,70 @@ const loginUser = async (req, res) => {
 
     const isMatch = await bcrypt.compare(password, user.password);
 
-    if(!isMatch){
-      return res.status(401).json({message:"Password Incorrect"});
-
+    if (!isMatch) {
+      return res.status(401).json({ message: "Password Incorrect" });
     }
 
-    const token=createToken(user._id);
+    const token = createToken(user._id);
 
-    return res.status(200).json({accessToken:token,user});
+    return res.status(200).json({ accessToken: token, user });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({message:"Error occured in login"})
-
-
-
+    return res.status(500).json({ message: "Error occured in login" });
   }
 };
 
-
-const getUser=async (req,res)=>{
+const getUser = async (req, res) => {
   try {
+    const user = await User.findById(req.userId).select("-password");
 
-    const user=await User.findById(req.userId).select("-password");
-
-    if(!user){
-      return res.status(404).json({message:"User not found"})
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
 
-    return res.status(200).json({user});
-
-
-
-    
+    return res.status(200).json({ user });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({message:"Server error"});
-    
+    return res.status(500).json({ message: "Server error" });
   }
-}
+};
 
-export { signUpUser,loginUser,getUser };
+const changePassword = async (req, res) => {
+  try {
+    const { currentPassword, newPassword } = req.body;
+
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "User Failed in Change Password" });
+    }
+
+    const isCurrentPasswordVerified = await bcrypt.compare(
+      currentPassword,
+      user.password,
+    );
+
+    if (!isCurrentPasswordVerified) {
+      return res
+        .status(400)
+        .json({ message: "Entered wrong current password" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+    user.password = hashedPassword;
+
+    await user.save();
+
+    return res.status(200).json({ message: "Password Changed Successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Failed to Change Password" });
+  }
+};
+
+export { signUpUser, loginUser, getUser,changePassword };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { signUpUser ,loginUser, getUser} from "../controllers/userController.js";
+import { signUpUser ,loginUser, getUser, changePassword} from "../controllers/userController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 const userRouter=express.Router();
 
 userRouter.post("/signup",signUpUser);
 userRouter.post("/login",loginUser);
-userRouter.get("/getuser",authMiddleware,getUser)
+userRouter.get("/getuser",authMiddleware,getUser);
+userRouter.post("/changepassword",authMiddleware,changePassword);
 
 
 export default userRouter;
