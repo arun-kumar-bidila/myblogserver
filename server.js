ade3d25a9048a1b504da601b11d3981182360aac
Arun Kumar Bidila <arunkumarbidila@gmail.com>
2026-02-23 13:44:23 +0530

    Revert "sign up using verify email"

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
-import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import cors from "cors";
+import cors from "cors"
 import userRouter from "./routes/userRoutes.js";
 import blogRouter from "./routes/blogRoutes.js";
 
-//CONFIG
 
+
+//CONFIG
+dotenv.config();
 connectDB();
-const app = express();
+const app=express();
 
 //MIDDLEWARES
 app.use(express.json());
 app.use(cors());
 
+
 //TEST API
-app.get("/test", (req, res) => {
-  return res.send("Server is healthy");
+app.get("/test",(req,res)=>{
+    return res.send("Server is healthy");
 });
+
+
 
 //ROUTES
-app.use("/api/auth", userRouter);
-app.use("/api/blog", blogRouter);
+app.use("/api/auth",userRouter);
+app.use("/api/blog",blogRouter)
 
-app.listen(process.env.PORT, "0.0.0.0", () => {
-  console.log("Server Running Successfully on port :" + process.env.PORT);
-});
+app.listen(process.env.PORT,"0.0.0.0",()=>{
+    console.log("Server Running Successfully on port :"+process.env.PORT);
+})
