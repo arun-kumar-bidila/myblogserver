e7660d14159475315336cf20663f3a645bd1d583
Arun Kumar Bidila <arunkumarbidila@gmail.com>
2026-03-20 12:57:32 +0530

    feat:add fcm token user field and api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signUpUser ,loginUser, getUser, changePassword} from "../controllers/userController.js";
+import { signUpUser ,loginUser, getUser, changePassword, saveFcmToken} from "../controllers/userController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 const userRouter=express.Router();
@@ -8,6 +8,7 @@ userRouter.post("/signup",signUpUser);
 userRouter.post("/login",loginUser);
 userRouter.get("/getuser",authMiddleware,getUser);
 userRouter.post("/changepassword",authMiddleware,changePassword);
+userRouter.post('/savefcmtoken',authMiddleware,saveFcmToken);
 
 
 export default userRouter;
